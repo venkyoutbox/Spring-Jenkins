283b17e54e21e01ced03296a08f2f4c6e4dae0c7
venky <venky@LAPTOP-56NQ90PA>
2021-04-24 13:57:31 +0800

    updated return message

--- Spring-Jenkins/src/main/java/com/example/demo/com/example/demo/SpringJenkinsApplication.java
+++ Spring-Jenkins/src/main/java/com/example/demo/com/example/demo/SpringJenkinsApplication.java
@@ -6,8 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringJenkinsApplication {
 		
 		public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
@@ -21,5 +24,17 @@ public class SpringJenkinsApplication {
 			logger.info("Application executed ......"); 
 			SpringApplication.run(SpringJenkinsApplication.class, args);
 		}
+		
+	  
+	  @RequestMapping("/getAll")
+	  public String test() {
+		  System.out.println("Welcome to YouTube...");
+		  return "Welcome to YouTube...";
+	  }
+	
+	
+	
+		
+		
 		
 }
